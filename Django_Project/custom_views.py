b7ff98fffb70e851ecba6c8896103d4c772bcaa4
irfaan <theirfaan07@gmail.com>
2024-06-22 20:57:06 +0530

    user auth

---
+++ Django_Project/custom_views.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.views import LogoutView
+
+class CustomLogoutView(LogoutView):
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
